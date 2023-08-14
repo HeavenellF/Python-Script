44a50185aaf81f583876c0f44c85bb13785872d7
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-14 17:01:24 +0200

    add txttojson.py
    this Script turns .txt file to .json file
    from Player1 Greeting1 to { "Player1" : "Greeting1"}

---
+++ txttojson.py
@@ -0,0 +1,16 @@
+import json
+
+with open('greeting.txt', 'r') as greeting_in_txt:
+    lines = greeting_in_txt.readlines()
+
+greeting_data = {}
+for line in lines:
+    parts = line.strip().split(' ', 1) 
+    if len(parts) == 2:
+        name, greeting = parts
+        greeting_data[name] = greeting
+
+with open('greeting.json', 'w') as greeting_in_json:
+    json.dump(greeting_data, greeting_in_json, indent=3)
+
+print(greeting_data)
