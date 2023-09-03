0a9295189b2bcbacbcd2716bd2e714e0faa45d9b
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-03 18:08:46 +0200

    added sys.exit(1) for critical error conditions

--- FileTypeSorter.py
+++ FileTypeSorter.py
@@ -5,7 +5,15 @@ import sys
 if len(sys.argv) != 2 :
     print("need 1 Argument")
     print("Usage : python FileTypeSorter.py <dir_path>")
+    sys.exit(1)
 
 dir_path = sys.argv[1]
+subDir_Doc = 'Documents'
+subDir_Img = 'Images'
+subDir_Txt = 'Texts'
+
+if not os.path.exists(dir_path):
+    print(f"{dir_path} doesnt exist")
+    sys.exit(1)
 
 print("Script Complete!")
