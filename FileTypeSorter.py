f76bae94ce03c0ac0485a0ec8a3aa87fc525c9e8
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-03 18:20:54 +0200

    added file iteration loop for file processing

--- FileTypeSorter.py
+++ FileTypeSorter.py
@@ -11,7 +11,9 @@ dir_path = sys.argv[1]
 subDir_Doc = 'Documents'
 subDir_Img = 'Images'
 subDir_Txt = 'Texts'
-
+imgFormat = ['.jpg', '.png', '.jpeg']
+txtFormat = ['.txt']
+docFormat = ['.wordx', '.pdf']
 if not os.path.exists(dir_path):
     print(f"{dir_path} doesnt exist")
     sys.exit(1)
@@ -23,5 +25,13 @@ for subDir in [subDir_Doc, subDir_Img, subDir_Txt]:
         print(f"create {subDir} in {dir_path}")
     else :
         print(f"{subDir} in {dir_path} already exists")
+i = 1
+for item in os.listdir(dir_path):
+    file = os.path.join(dir_path, item)
+    if os.path.isfile(file):
+        print(i)
+        i += 1
+
+
 
 print("Script Complete!")
