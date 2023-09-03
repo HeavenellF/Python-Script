0ea686f8f6cfc9fc77a5dfdec9b029f608f4eb66
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-03 17:49:29 +0200

    added command-line argument handling

---
+++ FileTypeSorter.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+import sys
+
+if len(sys.argv) != 2 :
+    print("need 1 Argument")
+    print("Usage : python FileTypeSorter.py <dir_path>")
+
+dir_path = sys.argv[1]
+
+print("Script Complete!")
